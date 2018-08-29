f238d3ee1f42760092fcb233fa20fdd33e6c7501
Jose Santiago <jose@binary-dev.com>
2018-08-28 21:11:35 -0400

    Removed some phantom files that were left behind by the git commit squashing

--- xavier-frontend/src/index.js
+++
@@ -1,6 +0,0 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import 'styles/index.css';
-import XavierApp from 'components/XavierApp';
-
-ReactDOM.render(<XavierApp />, document.getElementById('root'));
